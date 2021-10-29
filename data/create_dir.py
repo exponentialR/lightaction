cc4b9f7131bd7c9aea712d8d4d781c0a368dc75b
exponentialR <sadebayo01@qub.ac.uk>
2021-10-29 20:17:23 +0100

    Updated the create_dir.py to dynamically store the data and include a dictionary key for the data headers

--- data/create_dir.py
+++ data/create_dir.py
@@ -1,10 +1,7 @@
 # from handdata import *
 import argparse
-import re
-import logging
-import traceback
+
 import os
-import sys
 import json
 from pathlib import Path
 
@@ -27,12 +24,11 @@ parser.add_argument("-len", "--video_length", help='The length of the video in f
 args = parser.parse_args()
 
 
-
 def create_dir(actions, subfolder_numbers, data_dir):
     """This function creates the data folder and corresponding subfolders,
     as defined by you on the command line. You can have as many data instances and subfolders as possible
     The file write logic on the last few lines overwrites the .iams file everytime you run this code
-    which is correspondingly used in handdata.py"""
+    which is correspondingly used in handdata.py. This function also """
 
     # data_dir = 'handdata'
     i = 1
@@ -53,17 +49,60 @@ def create_dir(actions, subfolder_numbers, data_dir):
                 pass
             finally:
                 # w = '{}.iams'.format(data_dir)
+
+                hand_landmark = {0: 'WRIST', 1: 'THUMB_CMC',
+                                 2: 'THUMB_MCP',
+                                 3: 'THUMB_IP',
+                                 4: 'THUMB_TIP',
+                                 5: 'INDEX_FINGER_MCP',
+                                 6: 'INDEX_FINGER_PIP',
+                                 7: 'INDEX_FINGER_DIP',
+                                 8: 'INDEX_FINGER_TIP',
+                                 9: 'MIDDLE_FINGER_MCP',
+                                 10: 'MIDDLE_FINGER_PIP',
+                                 11: 'MIDDLE_FINGER_DIP',
+                                 12: 'MIDDLE_FINGER_TIP',
+                                 13: 'RING_FINGER_MCP',
+                                 14: 'RING_FINGER_PIP',
+                                 15: 'RING_FINGER_DIP',
+                                 16: 'RING_FINGER_TIP',
+                                 17: 'PINKY_MCP',
+                                 18: 'PINKY_PIP',
+                                 19: 'PINKY_DIP',
+                                 20: 'PINKY_TIP'}
+
+                empty_list = []
+                for k, v in hand_landmark.items():
+                    for i in range(3):
+                        empty_list.append(v)
+
+                hand_landmark_x_y_z = [['{}_x'.format(x), '{}_y'.format(y), '{}_y'.format(z)] for x, y, z in
+                                       zip(empty_list[0::3], empty_list[1::3], empty_list[2::3])]
+                hand_landmark_x_y_z = [item for sublist in hand_landmark_x_y_z for item in sublist]
+
                 iams_dict = {'actions': args.actions, 'subfolder_length': args.subfolder_numbers,
                              'video_length': args.video_length, 'Data_Directory': data_dir,
-                             'Data_Subfolder': str(Path("{}/data".format(args.pat)))}
+                             'Data_Subfolder': str(Path("{}/data".format(args.pat))), 'csv_header': hand_landmark_x_y_z}
+
+                # print(len(hand_landmark_x_y_z))
+                # hand_dict = {}
+                # for i in range(63):
+                #     hand_dict[i] = hand_landmark_x_y_z[i]
 
                 w = 'iamsMediapipe.iams'
-                with open(w, 'r++') as f:
+                with open(w, 'r+') as f:
                     f.seek(0)
                     json.dump(iams_dict, f)
                     f.truncate()
 
                 f.close()
+
+                # w_ = 'csv_header.iams'
+                # with open(w_, 'w') as header:
+                #     json.dump(hand_dict, header)
+                #
+                # header.close()
+
     print(data_dir)
 
 
--- handdata.py
+++ handdata.py
@@ -10,7 +10,16 @@ mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic  # Drawing utilities
 
 
-class sequential_data:
+def Detection_pipeline(image, holistic):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False  # Image is no longer writeable
+    coords = holistic.process(image)  # Make prediction
+    image.flags.writeable = True  # Image is now writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
+    return image, coords
+
+
+class human_pose_data:
     def __init__(self):
         # self.states, self.pathdir, self.len_seq, self.len_fold = make_sequence_folder()
         self.solutions = mp.solutions.holistic
@@ -20,14 +29,6 @@ class sequential_data:
         self.pose_connect = self.solutions.POSE_CONNECTIONS
         self.face_connect = self.solutions.FACEMESH_TESSELATION
         # self.hands = mp_hands
-
-    def Detection_pipeline(self, image, holistic):
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
-        image.flags.writeable = False  # Image is no longer writeable
-        coords = holistic.process(image)  # Make prediction
-        image.flags.writeable = True  # Image is now writeable
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
-        return image, coords
 
     def draw_landmarks(self, image, coords):
         self.draw.draw_landmarks(image, coords.left_hand_landmarks, self.hand_connect)  # Draw left hand connections
@@ -49,24 +50,34 @@ class sequential_data:
                 if coords.multi_hand_landmarks:
 
                     for idx, hand_landmark in enumerate(coords.multi_hand_landmarks):
-
                         self.draw.draw_landmarks(image, hand_landmark, mp_hands.HAND_CONNECTIONS,
-                                                 self.draw.DrawingSpec(color=(255, 0, 200), thickness=1, circle_radius=3),
-                                                 self.draw.DrawingSpec(color=(150, 120, 255), thickness=1, circle_radius=2), )
+                                                 self.draw.DrawingSpec(color=(255, 0, 200), thickness=1,
+                                                                       circle_radius=3),
+                                                 self.draw.DrawingSpec(color=(150, 120, 255), thickness=1,
+                                                                       circle_radius=2), )
                     for _, classification in enumerate(coords.multi_handedness):
-                        if classification.classification[0].index == idx and classification.classification[0].index ==1:
+                        if classification.classification[0].index == idx and classification.classification[
+                            0].index == 1:
                             hand_label = classification.classification[0].label
                             coord = tuple(
-                                np.multiply(np.array((hand_landmark.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x, hand_landmark.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y)), [640, 480]).astype(int))
+                                np.multiply(np.array((hand_landmark.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x,
+                                                      hand_landmark.landmark[
+                                                          mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y)),
+                                            [640, 480]).astype(int))
                             text = '{} {}'.format(hand_label, coord)
-                            cv2.putText(image, text, coord, cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 100, 200), 2, cv2.LINE_AA)
+                            cv2.putText(image, text, coord, cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 100, 200), 2,
+                                        cv2.LINE_AA)
 
                         elif classification.classification[0].index == idx:
                             hand_label = classification.classification[0].label
                             coord = tuple(
-                                np.multiply(np.array((hand_landmark.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x, hand_landmark.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y)), [640, 480]).astype(int))
+                                np.multiply(np.array((hand_landmark.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x,
+                                                      hand_landmark.landmark[
+                                                          mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y)),
+                                            [640, 480]).astype(int))
                             text = '{} {}'.format(hand_label, coord)
-                            cv2.putText(image, text, coord, cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 100, 200), 2, cv2.LINE_AA)
+                            cv2.putText(image, text, coord, cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 100, 200), 2,
+                                        cv2.LINE_AA)
                         else:
                             # classification.classification[0].index == 1:
                             hand_label = classification.classification[0].label
@@ -80,12 +91,11 @@ class sequential_data:
                                         cv2.LINE_AA)
                 cv2.imshow('Output Hand-Tracking', image)
                 if cv2.waitKey(10) != ord('q'):
-
                     continue
                 camera.release()
                 cv2.destroyAllWindows()
 
-    def Draw_only_face(self):
+    def _only_face(self):
 
         camera = cv2.VideoCapture(0)
 
@@ -93,7 +103,7 @@ class sequential_data:
             while True:
                 ret, image = camera.read()
                 image = cv2.flip(image, 1)
-                frame, coords = self.Detection_pipeline(image, holistic)
+                frame, coords = Detection_pipeline(image, holistic)
                 self.draw.draw_landmarks(image, coords.face_landmarks, self.face_connect)
                 cv2.imshow('Output_Feed Facial', frame)
 
@@ -102,15 +112,14 @@ class sequential_data:
                 camera.release()
                 cv2.destroyAllWindows()
 
-
-    def Draw_only_pose(self):
+    def _only_pose(self):
         camera = cv2.VideoCapture(0)
 
         with self.holistic as holistic:
             while True:
                 ret, image = camera.read()
                 image = cv2.flip(image, 1)
-                frame, coords = self.Detection_pipeline(image, holistic)
+                frame, coords = Detection_pipeline(image, holistic)
                 self.draw.draw_landmarks(image, coords.pose_landmarks, self.pose_connect)
                 cv2.imshow('Output_Feed Facial', frame)
 
@@ -119,22 +128,24 @@ class sequential_data:
                 camera.release()
                 cv2.destroyAllWindows()
 
+    @staticmethod
+    def extract_full_body(coords):
+        if coords.multi_hand_landmarks:
+            """Function to extract coordinates of keypoints on both hands"""
+            right_hand = np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
+                                   coords.right_hand_landmarks.landmark]).flatten() if coords.right_hand_landmarks else np.zeros(
+                21 * 3)
+            left_hand = np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
+                                  coords.left_hand_landmarks.landmark]).flatten() if coords.left_hand_landmarks else np.zeros(
+                21 * 3)
+            pose = np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
+                             coords.pose_landmarks]).flatten() if coords.pose_landmarks else np.zeros(132)
+            facial = np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
+                               coords.face_landmarks]).flatten() if coords.face_landmarks else np.zeros(1)
+            return np.concatenate([right_hand, left_hand, pose, facial])
 
-    def extract_full_body(self, coords):
-        """Function to extract coordinates of keypoints on both hands"""
-        right_hand = np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
-                               coords.right_hand_landmarks.landmark]).flatten() if coords.right_hand_landmarks else np.zeros(
-            21 * 3)
-        left_hand = np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
-                              coords.left_hand_landmarks.landmark]).flatten() if coords.left_hand_landmarks else np.zeros(
-            21 * 3)
-        pose = np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
-                         coords.pose_landmarks]).flatten() if coords.pose_landmarks else np.zeros(132)
-        facial = np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
-                           coords.face_landmarks]).flatten() if coords.face_landmarks else np.zeros(1)
-        return np.concatenate([right_hand, left_hand, pose, facial])
-
-    def extract_only_hands(self, coords):
+    @staticmethod
+    def extract_only_hands(coords):
         """this function extracts only coordinates of the hands"""
         right_hand = np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
                                coords.right_hand_landmarks.landmark]).flatten() if coords.right_hand_landmarks else np.zeros(
@@ -144,33 +155,23 @@ class sequential_data:
             21 * 3)
         return np.concatenate([left_hand, right_hand])
 
-    def extract_only_facial(self, coords):
+    @staticmethod
+    def extract_only_facial(coords):
         return np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
                          coords.face_landmarks]).flatten() if coords.face_landmarks else np.zeros(1)
 
-    def extract_only_pose(self, coords):
+    @staticmethod
+    def extract_only_pose(coords):
         return np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
                          coords.pose_landmarks]).flatten() if coords.pose_landmarks else np.zeros(132)
 
-        # lh, rh = [], []
-        # for ids, hand_landmarks in enumerate(results.multi_hand_landmarks):
-        #     if ids == 0:
-        #         lh = np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
-        #                        hand_landmarks.landmark]).flatten() if True else np.zeros(21 * 3)
-        #     if ids == 1:
-        #         rh = np.array([[landmarked.x, landmarked.y, landmarked.z] for landmarked in
-        #                        hand_landmarks.landmark]).flatten() if True else np.zeros(21 * 3)
-        # x = np.concatenate([lh, rh])
-        # return x.tolist()
-        #     return np.concatenate([lh, rh])
-
-    def Draw_whole_body(self):
+    def _whole_body(self):
         camera = cv2.VideoCapture(0)
         with self.holistic as holistic:
             while True:
                 ret, image = camera.read()
                 image = cv2.flip(image, 1)
-                frame, coords = self.Detection_pipeline(image, holistic)
+                frame, coords = Detection_pipeline(image, holistic)
                 self.draw_landmarks(frame, coords)
                 cv2.imshow('Output_Feed Whole Body', frame)
 
@@ -179,7 +180,7 @@ class sequential_data:
                 camera.release()
                 cv2.destroyAllWindows()
 
-#
+    #
     def collect_data(self):
         camera = cv2.VideoCapture(0)
         """
@@ -187,7 +188,7 @@ class sequential_data:
         Get the list of actions
         get the number of subfolders
         iterate through them"""
-        cur_scr_dir = os.path.dirname(__file__) #This is the absolute directory the script is in
+        cur_scr_dir = os.path.dirname(__file__)  # This is the absolute directory the script is in
         relative_data_path = "data/iamsMediapipe.iams"
         abs_iams_path = Path(os.path.join(cur_scr_dir, relative_data_path))
 
@@ -195,21 +196,21 @@ class sequential_data:
         f_content = json.load(f)
         with self.holistic as holistic:
             for action in f_content['actions']:
-                for folder in range(0, len(f_content['subfolder_length'])):
-                    for img_ in range(len(f_content['video_length'])):
+                for folder in range(f_content['subfolder_length']):
+                    for img_ in range(f_content['video_length']):
                         _, image = camera.read()
-                        image, coords = self.Detection_pipeline(image, holistic)
+                        image, coords = Detection_pipeline(image, holistic)
                         if img_ == 0:
                             cv2.putText(image, 'Starting Data Collection into {}'.format(f_content['Data_Directory']),
-                                        (100, 100), cv2.FarnebackOpticalFlow, 1, (120, 100, 150), 3, cv2.LINE_AA)
+                                        (100, 100), cv2.FONT_HERSHEY_TRIPLEX, 1, (120, 100, 150), 3, cv2.LINE_AA)
                             cv2.putText(image, 'Now Collecting Data for {}; {}'.format(action, folder),
-                                        (15, 12), cv2.FarnebackOpticalFlow, 1, (120, 100, 150), 3, cv2.LINE_AA)
+                                        (15, 12), cv2.FONT_HERSHEY_TRIPLEX, 1, (120, 100, 150), 3, cv2.LINE_AA)
                             cv2.imshow('Data Collection Feed', image)
                             cv2.waitKey(2000)
 
                         else:
                             cv2.putText(image, 'Now Collecting Data for {}; {}'.format(action, folder),
-                                        (15, 12), cv2.FarnebackOpticalFlow, 1, (120, 100, 150), 3, cv2.LINE_AA)
+                                        (15, 12), cv2.FONT_HERSHEY_PLAIN, 1, (120, 100, 150), 3, cv2.LINE_AA)
                             cv2.imshow('Data Collection Feed', image)
 
                         keypoints = self.extract_full_body(coords)
